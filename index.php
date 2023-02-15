46a2ce7550e25032a76530644b65d31aad28973f
safoora <safoura.mansouri@gmail.com>
2023-02-15 11:24:25 -0500

    final

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
 <?php get_header();?>
-<!-- <nav>
-    <ul>
-        <li>Home</li>
-        <li>about us</li>
-        <li>experience</li>
-        <li>contact</li>
-    </ul>
-</nav> -->
+
 <main>
     <h3>exercice1</h3>
     <h2>this is the first page of my try</h2>
 <?php
 if (have_posts()):
      while (have_posts()) : the_post();
-         the_title('<h1>','<h1>');
-          the_content();
+       // the_title('<h1>','<h1>');
+          //the_content();?>
+          <h1><a href="<?php the_permalink();?>"><?= get_the_title() ?></a></h1>
+          <?php
+
         // the_excerpt();
         echo wp_trim_words(get_the_excerpt(), 4);
     endwhile;
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+
+Safoora Mansouri
+
+aecweb16.sg-host.com/
+
+Github:
+https://github.com/Safoora-Mansouri/exercice1
